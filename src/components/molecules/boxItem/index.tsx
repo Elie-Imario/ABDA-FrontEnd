afd95fd107b056f41d5a5857e228771644912015
elie-imario <elieimario1@gmail.com>
2024-02-12 18:26:55 +0300

    remove iconName props from boxComponent

--- src/components/molecules/boxItem/index.tsx
+++ src/components/molecules/boxItem/index.tsx
@@ -4,18 +4,17 @@ import './BoxItem.scss'
 
 type Props = {
     label: string,
-    iconname : string,
     data : number
 }
 
-const BoxItem:FC<Props> = ({label, iconname, data}) => {
+const BoxItem:FC<Props> = ({label, data}) => {
   return (
     <div className="box-item">
         <div className="box-header">
             <div>
                 <span className="box-title-lead">{label}</span>
             </div>
-            <span><FontAwesomeIcon icon={iconname} size='lg'/></span>
+            <span><FontAwesomeIcon icon={label.toLowerCase() === 'insertion' ? 'check-circle' : label.toLowerCase() === 'modification' ? 'edit' : 'trash-can' } size='lg'/></span>
         </div>
         <div className="box-body">
             <span>{data}</span>
