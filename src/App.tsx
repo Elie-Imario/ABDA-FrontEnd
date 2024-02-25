7c9cfcefe27b052ffe46b59627843fb7f99eac04
elie-imario <elieimario1@gmail.com>
2024-02-25 22:16:27 +0300

    move app router to app main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import PageRouter from "./Pages/PageRouter";
+import Audit from "./Pages/Audit";
+import Inscription from "./Pages/Inscription";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import { SideBarWrapper, MainSide } from "./plugins/globalStyles";
+import LeftSide from "./Layout/LeftSide";
 import "./plugins/font-plugin-kit.js";
 import "./plugins/fa-plugin-kit.js";
 import "./App.scss";
+import "./Pages/Pages.scss";
 
 const Main = () => {
   return (
     <div className="App">
-      <PageRouter />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/inscriptions"
+            element={
+              <>
+                <SideBarWrapper>
+                  <LeftSide />
+                </SideBarWrapper>
+                <MainSide className="right-side">
+                  <Inscription />
+                </MainSide>
+              </>
+            }
+          />
+          <Route
+            path="/audits"
+            element={
+              <>
+                <SideBarWrapper>
+                  <LeftSide />
+                </SideBarWrapper>
+                <MainSide className="right-side">
+                  <Audit />
+                </MainSide>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
