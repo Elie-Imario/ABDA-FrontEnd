d0d32b86c0370d0dfa0038b22d47076f97e4e4ac
elie-imario <elieimario1@gmail.com>
2024-02-25 16:46:06 +0300

    add provider for UserLogContext into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import Layout from "./Layout";
 import Pages from "./Pages/index.js";
+import { useContext } from "react";
+import { AppContext } from "./service/context/index.js";
 import Login from "./Pages/Login/index.js";
 import "./plugins/font-plugin-kit.js";
 import "./plugins/fa-plugin-kit.js";
 import "./App.scss";
 
 const Main = () => {
+  const { UserLogContext } = useContext(AppContext);
   return (
     <div className="App">
-      {
+      {UserLogContext ? (
+        <Layout>
+          <Pages />
+        </Layout>
+      ) : (
         <Login />
-        // <Layout>
-        //   <Pages />
-        // </Layout>
-      }
+      )}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppProvider from "./service/context/index.tsx";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
