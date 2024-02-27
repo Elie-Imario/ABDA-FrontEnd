0f6ac6f8942d0dae9f128ee30bd93d8d16e51e52
elie-imario <elieimario1@gmail.com>
2024-02-27 06:54:36 +0300

    remove strict mode in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import AppProvider from "./service/context/index.tsx";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
+  <AppProvider>
+    <App />
+  </AppProvider>
 );
