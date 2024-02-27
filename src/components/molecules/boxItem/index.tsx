633f54986c574a47c46baca7aa81873900f36d1a
elie-imario <elieimario1@gmail.com>
2024-02-27 08:06:50 +0300

    diplay boxitem label

--- src/components/molecules/boxItem/index.tsx
+++ src/components/molecules/boxItem/index.tsx
@@ -1,27 +1,43 @@
-import { FC } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './BoxItem.scss'
+import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./BoxItem.scss";
 
 type Props = {
-    label: string,
-    data : number
-}
+  label: string;
+  data: number;
+};
 
-const BoxItem:FC<Props> = ({label, data}) => {
+const BoxItem: FC<Props> = ({ label, data }) => {
   return (
     <div className="box-item">
-        <div className="box-header">
-            <div>
-                <span className="box-title-lead">{label}</span>
-            </div>
-            <span><FontAwesomeIcon icon={label.toLowerCase() === 'insertion' ? 'check-circle' : label.toLowerCase() === 'modification' ? 'edit' : 'trash-can' } size='lg'/></span>
+      <div className="box-header">
+        <div>
+          <span className="box-title-lead">
+            {label.toLowerCase() === "add"
+              ? "insertion"
+              : label.toLowerCase() === "mod"
+              ? "modification"
+              : "suppression"}
+          </span>
         </div>
-        <div className="box-body">
-            <span>{data}</span>
-        </div>
+        <span>
+          <FontAwesomeIcon
+            icon={
+              label.toLowerCase() === "add"
+                ? "check-circle"
+                : label.toLowerCase() === "mod"
+                ? "edit"
+                : "trash-can"
+            }
+            size="lg"
+          />
+        </span>
+      </div>
+      <div className="box-body">
+        <span>{data}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default BoxItem; 
+export default BoxItem;
